a4b090bf6aea14454999afc61dd1becf64606db1
leentj0201 <leentj0201@gmail.com>
2021-11-09 10:13:39 +0900

    application-real.properties

--- src/test/java/simflow/praycard/PraycardApplicationTests.java
+++ src/test/java/simflow/praycard/PraycardApplicationTests.java
@@ -3,7 +3,7 @@ package simflow.praycard;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PraycardApplicationTests {
 
     @Test
